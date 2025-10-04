8dfef26a5fff350b65a1110979df64fc336ca097
Lucas Cavalcante de Sousa <lucks.sousa@gmail.com>
2025-10-04 13:37:54 -0300

    feat: add 3rd person camera

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody3D
 
 
+@onready var camera : Camera3D = $SpringArmPivot/Camera3D
+
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
@@ -18,6 +20,8 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction = direction.rotated(Vector3.UP, camera.global_rotation.y)
+
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
