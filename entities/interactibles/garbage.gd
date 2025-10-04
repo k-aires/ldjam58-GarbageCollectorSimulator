20edfc6520697ab429342871934e2d385d07870e
k_aires <lenakaires@gmail.com>
2025-10-04 11:40:31 -0300

    add type checking to garbage class

--- entities/interactibles/garbage.gd
+++ entities/interactibles/garbage.gd
@@ -5,10 +5,20 @@ extends RigidBody3D
 @export_group("Garbage Properties")
 @export_range(0,10,1.0,"or_greater") var max_capacity: int = 1
 
+var capacity: int = 1 :
+	set = set_capacity
 
-func interact():
+
+func interact() -> void:
 	pass
 
 
-func collect():
+func collect() -> void:
 	pass
+
+
+func set_capacity(value: int) -> void:
+	if value >= 0:
+		capacity = value
+	else:
+		capacity = 0
