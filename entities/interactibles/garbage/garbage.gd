83b01816ac29e8b642cf1301cc78b7d56cc5ef45
k_aires <lenakaires@gmail.com>
2025-10-05 14:57:09 -0300

    add check to garbage script for label not appearing when storage empty

--- entities/interactibles/garbage/garbage.gd
+++ entities/interactibles/garbage/garbage.gd
@@ -2,15 +2,13 @@ class_name Garbage
 extends RigidBody3D
 
 
-@export_group("Garbage Properties")
-@export_range(0,10,1.0,"or_greater") var max_capacity: int = 1
 @onready var label: Label3D = $Label3D
 
 func _ready() -> void:
 	set_label(false)
 
 func set_label(visible: bool) -> void:
-	label.visible = visible
+	label.visible = visible and not $Storage.is_empty()
 
 func get_storage() -> Storage:
 	return $Storage
