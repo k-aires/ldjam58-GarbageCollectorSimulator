3d5b84264daf61633095bfb91fca3977c0bfb354
k_aires <lenakaires@gmail.com>
2025-10-05 15:20:56 -0300

    add random garbage amount to storage on ready

--- entities/interactibles/garbage/garbage.gd
+++ entities/interactibles/garbage/garbage.gd
@@ -3,12 +3,18 @@ extends RigidBody3D
 
 
 @onready var label: Label3D = $Label3D
+@onready var storage: Storage = $Storage
+
 
 func _ready() -> void:
 	set_label(false)
+	var random_storage = randi_range(1, storage.check_empty_space())
+	storage.add_to_storage(random_storage)
+
 
 func set_label(visible: bool) -> void:
-	label.visible = visible and not $Storage.is_empty()
+	label.visible = visible and not storage.is_empty()
+
 
 func get_storage() -> Storage:
-	return $Storage
+	return storage
